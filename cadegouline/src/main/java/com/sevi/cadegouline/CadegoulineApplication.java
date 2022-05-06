5896413c49896c4eb86cc1e1fc0548ae5d027206
sebas <sebastien.viot.49@gmail.com>
2022-05-06 16:01:10 +0200

    Essai installation ffmpeg

--- cadegouline/src/main/java/com/sevi/cadegouline/CadegoulineApplication.java
+++ cadegouline/src/main/java/com/sevi/cadegouline/CadegoulineApplication.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 @SpringBootApplication
 public class CadegoulineApplication {
 	public static void main(String[] args) throws IOException {
-		Runtime.getRuntime().exec("sudo apt-get install ffmpeg");
+		Runtime.getRuntime().exec("apt-get install ffmpeg");
 		SpringApplication.run(CadegoulineApplication.class, args);
 	}
 }
